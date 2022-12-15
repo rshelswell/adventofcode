66f0f700aaccc3e09a1b39d27a222de2b0ae700a
Richard Shelswell <github@richardshelswell.co.uk>
2022-12-15 13:50:24 +0000

    Finished day 4, added day 5 input
    Signed-off-by: rshelswell <github@richardshelswell.co.uk>

--- 2022/day4.py
+++ 2022/day4.py
@@ -1,4 +1,29 @@
+import re
 def pt1():
-    pass
-print(pt1())
-# print(pt2())
+    count = 0
+    with open('day4.input', 'r') as f:
+        for l in f:
+            bounds = re.split(r'[,-]',l.strip())
+            bounds = [int(x) for x in bounds]
+            print(bounds)
+            if (bounds[0] <= bounds[2] and bounds[1] >= bounds[3]) \
+                or (bounds[2] <= bounds[0] and bounds[3] >= bounds[1]):
+                count += 1
+    return count
+
+def pt2():
+    count = 0
+    with open('day4.input', 'r') as f:
+        for l in f:
+            bounds = re.split(r'[,-]',l.strip())
+            bounds = [int(x) for x in bounds]
+            # print(bounds)
+            a = set(range(bounds[0],bounds[1]+1))
+            b = set(range(bounds[2],bounds[3]+1))
+            # print(a, b)
+            if a & b:
+                count += 1
+    return count
+
+print('part 1: ', pt1())
+print('part 2: ', pt2())
