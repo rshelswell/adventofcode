a6bccf1914cfe3b8cdf608c47ff78d5de4f9ee11
Richard Shelswell <github@richardshelswell.co.uk>
2022-12-16 16:27:43 +0000

    Continued day 7
    Signed-off-by: rshelswell <github@richardshelswell.co.uk>

--- 2022/day7.py
+++ 2022/day7.py
@@ -14,10 +14,21 @@ class Directory:
     def get_size(self):
         return self.size
 
-def pt1():
+def pt2():
     with open('day7test.input', 'r') as f:
         for l in f:
-            #parse structure
+            # parse structure
+            print(l)
+            l = l.strip()
+            if l[0:4] == '$ cd':
+                # change directory command
+                if l[5:7] == '..':
+                    # go to parent directory
+                    pass
+                else:
+                    # change to child directory
+                    child_name = l[5:]
 
 
-print(pt1())
+# print(pt1())
+print(pt2())
